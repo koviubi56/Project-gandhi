2578a72b65243f17ef8e54005f97f671f0d69dc7
koviubi56 <>
2021-04-25 14:08:05 +0000

    milk

--- runme.sh
+++ runme.sh
@@ -1,10 +1,10 @@
-echo Updating pip
+echo ----------Updating pip
 pip install -U pip
-echo Installing dependencies
+echo ----------Installing dependencies
 pip install -r requirements.txt
-echo Checking discord
+echo ----------Checking discord
 pip check discord
-echo Checking requests
+echo ----------Checking requests
 pip check requests
 echo -_- RUNNING BOT -_-
 python ./main.py
