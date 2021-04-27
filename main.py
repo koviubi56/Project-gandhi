6cb48ade5bce56eb380915f2fc01fa4a83249fc9
koviubi56 <kmlenke1976@gmail.com>
2021-04-27 13:55:42 +0200

    print -> log

--- main.py
+++ main.py
@@ -7,33 +7,6 @@ If a copy of the MPL was not distributed with this file,
 You can obtain one at http://mozilla.org/MPL/2.0/.
 """
 
-try:
-    from replit import db
-except Exception as e:
-    print("[ERROR]")
-    print(f"Error when importin db from replit: {str(e)}")
-    print("Trying again...")
-    for e2 in range(5):
-        try:
-            from replit import db
-        except:
-            if e2 < 4:
-                print(f"[ERROR {e2} ]")
-            elif e2 >= 4:
-                print("*** CRITICAL ERROR! ***")
-                print("We can't import db from replit after 6 tries.")
-                print("The bot CAN'T work without it.")
-                print("Stopping...")
-                exit()
-try:
-    db["szerdak"] -= 1
-except:
-    db["szerdak"] = 0
-else:
-    db["szerdak"] += 1
-finally:
-    inSet = False
-
 while True:
     print("Starting...")
     try:
@@ -43,8 +16,34 @@ while True:
         import dc
         from logging import *
         import logging
-        logging.basicConfig(level=logging.debug, format="[%(name)s %(asctime)s %(levelname)s line: %(lineno)d] %(message)s")
-        logging.info("Starting...")
+        logging.basicConfig(
+            level=logging.debug, format="[%(name)s %(asctime)s %(levelname)s line: %(lineno)d] %(message)s")
+
+        try:
+            from replit import db
+        except Exception as e:
+            logging.error(f"Error when importin db from replit: {str(e)}")
+            logging.info("Trying again...")
+            for e2 in range(5):
+                try:
+                    from replit import db
+                except:
+                    if e2 < 4:
+                        logging.error(f"[ERROR {e2} ]")
+                    elif e2 >= 4:
+                        logging.critical(
+                            "We can't import db from replit after 6 tries. The bot CAN'T work without it.")
+                        logging.info("Stopping...")
+                        exit()
+        try:
+            db["szerdak"] -= 1
+        except:
+            db["szerdak"] = 0
+        else:
+            db["szerdak"] += 1
+        finally:
+            inSet = False
+
         # ===SETTINGS===
         # Mennyi a maximális szerda amennyit elfogad egy üzenethez.
         maxSzerda = 510
@@ -79,7 +78,7 @@ while True:
             try:
                 for i, betu in enumerate(content):
                     if plus > maxSzerda:
-                        print("TÚL SOK SZERDA!")
+                        logging.error("TÚL SOK SZERDA!")
                         await dc.send(
                             msg, f"Ez az üzenet annyira menő, hogy több mint {maxSzerda} szerda van benne.")
                         await dc.send(
@@ -120,7 +119,7 @@ while True:
             # stop
             if dc.cmd(os.environ['KEY'], 'stop', msg):
                 await dc.send(msg, f'Szerdák: {str(db["szerdak"])}')
-                print("Stopping...")
+                logging.info("Stopping...")
                 exit()
 
             # set
@@ -165,6 +164,5 @@ while True:
 
     except Exception as e:
         print("\n\n")
-        print("[ERROR]")
-        print(f"Error code: {str(e)}")
-        print("Restarting...")
+        logging.error(f"Error code: {str(e)}")
+        logging.info("Restarting...")
