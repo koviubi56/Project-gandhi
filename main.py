380b13897951c1c959a30a803fbe409ee6d6fee5
koviubi56 <kmlenke1976@gmail.com>
2021-04-27 11:16:25 +0200

    k

--- main.py
+++ main.py
@@ -7,9 +7,6 @@ If a copy of the MPL was not distributed with this file,
 You can obtain one at http://mozilla.org/MPL/2.0/.
 """
 
-from logging import log
-
-
 try:
     from replit import db
 except Exception as e:
@@ -39,63 +36,65 @@ finally:
 
 while True:
     print("Starting...")
-#    try:
-    import os
-    import discord
-    import random
-    import dc
-    import logging
-    logging.basicConfig(
-        level=logging.DEBUG, format="%(name)s  %(level)s  %(pathname)s  %(lineno)s  %(msg)s  %(args)s  %(exc_info)s  %(func)s  %(sinfo)s")
-    logging.DEBUG("hi")
-    # ===SETTINGS===
-    # Mennyi a maximális szerda amennyit elfogad egy üzenethez.
-    maxSzerda = 510
-    # Verzió
-    version = "1.3.0"
-    # Bármi más? "-beta.1"?
-    pre = "-beta.2"
-    # Prefix
-    prefix = "56!"
-    # ---SETTINGS---
+    try:
+        import os
+        import discord
+        import random
+        import dc
+        import logging
+        logging.basicConfig(
+            level=logging.DEBUG, format="%(name)s  %(level)s  %(pathname)s  %(lineno)s  %(msg)s  %(args)s  %(exc_info)s  %(func)s  %(sinfo)s")
+        logging.debug("hi")
+        # ===SETTINGS===
+        # Mennyi a maximális szerda amennyit elfogad egy üzenethez.
+        maxSzerda = 510
+        # Verzió
+        version = "1.3.0"
+        # Bármi más? "-beta.1"?
+        pre = "-beta.2"
+        # Prefix
+        prefix = "56!"
+        # ---SETTINGS---
 
-    print('{:=^63}'.format(f'GANDHI BOT {version}{pre}'))
+        print('{:=^63}'.format(f'GANDHI BOT {version}{pre}'))
 
-    client = discord.Client()
+        client = discord.Client()
 
-    @client.event
-    async def on_ready():
-        print('This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.')
-        print('Bejelentkezve: "{0.user}"'.format(client))
+        @client.event
+        async def on_ready():
+            print('This Source Code Form is subject to the terms of the Mozilla Public License, v. 2.0. If a copy of the MPL was not distributed with this file, You can obtain one at http://mozilla.org/MPL/2.0/.')
+            print('Bejelentkezve: "{0.user}"'.format(client))
 
-    @client.event
-    async def on_message(message):
-        if message.author == client.user:
-            return
+        @client.event
+        async def on_message(message):
+            if message.author == client.user:
+                return
 
-        plus = 0
-        msg = message
-        content = msg.content
+            plus = 0
+            msg = message
+            content = msg.content
 
-        # szerda keresés
-        try:
-            for i, betu in enumerate(content):
-                if plus > maxSzerda:
-                    print("TÚL SOK SZERDA!")
-                    await dc.send(
-                        msg, f"Ez az üzenet annyira menő, hogy több mint {maxSzerda} szerda van benne.")
-                    await dc.send(
-                        msg, "|| Megjegyzés magamnak: *set*; szerdák: {szerdak} ||".format(szerdak=str(db["szerdak"])))
-                    plus = 0
-                    break
-                if betu.lower() == "s":
-                    if content[i + 1] == "z":
-                        if content[i + 2] == "e":
-                            if content[i + 3] == "r":
-                                if content[i + 4] == "d":
-                                    if content[i + 5] == "a" or content[i + 5] == "á":
-                                        plus += 1
-                                        continue
+            # szerda keresés
+            try:
+                for i, betu in enumerate(content):
+                    if plus > maxSzerda:
+                        print("TÚL SOK SZERDA!")
+                        await dc.send(
+                            msg, f"Ez az üzenet annyira menő, hogy több mint {maxSzerda} szerda van benne.")
+                        await dc.send(
+                            msg, "|| Megjegyzés magamnak: *set*; szerdák: {szerdak} ||".format(szerdak=str(db["szerdak"])))
+                        plus = 0
+                        break
+                    if betu.lower() == "s":
+                        if content[i + 1] == "z":
+                            if content[i + 2] == "e":
+                                if content[i + 3] == "r":
+                                    if content[i + 4] == "d":
+                                        if content[i + 5] == "a" or content[i + 5] == "á":
+                                            plus += 1
+                                            continue
+                                        else:
+                                            continue
                                     else:
                                         continue
                                 else:
@@ -106,68 +105,65 @@ while True:
                             continue
                     else:
                         continue
-                else:
-                    continue
-        except IndexError:
-            pass
+            except IndexError:
+                pass
 
-        if plus > 0:
-            db["szerdak"] += plus
-            await dc.send(msg, "+{plus} szerda (most: {szerdak})".format(plus=plus, szerdak=db["szerdak"]))
+            if plus > 0:
+                db["szerdak"] += plus
+                await dc.send(msg, "+{plus} szerda (most: {szerdak})".format(plus=plus, szerdak=db["szerdak"]))
 
-        # 56
-        if content == '56':
-            await dc.send(msg, f'Szerdák száma: {str(db["szerdak"])}')
+            # 56
+            if content == '56':
+                await dc.send(msg, f'Szerdák száma: {str(db["szerdak"])}')
 
-        # stop
-        if dc.cmd(os.environ['KEY'], 'stop', msg):
-            await dc.send(msg, f'Szerdák: {str(db["szerdak"])}')
-            print("Stopping...")
-            exit()
+            # stop
+            if dc.cmd(os.environ['KEY'], 'stop', msg):
+                await dc.send(msg, f'Szerdák: {str(db["szerdak"])}')
+                print("Stopping...")
+                exit()
 
-        # set
-        global inSet
-        if inSet == True:
-            inSet = False
-            db["szerdak"] = content
+            # set
+            global inSet
+            if inSet == True:
+                inSet = False
+                db["szerdak"] = content
 
-        if dc.cmd(os.environ['KEY'], 'set', msg):
-            await dc.send(msg, 'K!')
-            inSet = True
+            if dc.cmd(os.environ['KEY'], 'set', msg):
+                await dc.send(msg, 'K!')
+                inSet = True
 
-        # 8ball
-        if dc.cmd(prefix, "8ball", msg):
-            lista8 = [
-                "ez%20pokolian%20nem",
-                "őszintén%20szólva%20nem%20érdekel%20lol",
-                "nem%20vagyok%20benne%20biztos,%20de te biztos,%20hogy%20hülye%20vagy",
-                "igen???",
-                "amikor%20növesztessz%20egy%20agysejtet,%20akkor%20igen",
-                "nem!!!!",
-                "lol%20szó%20szerint%20nem",
-                "a%20fenébe!%20nem.",
-                "persze%20miért%20ne",
-                "nem%20lmfao",
-                "persze,%20engem%20se%20érdekel%20jobban",
-                "Trump%20színe%20narancssárga?",
-                "én%20egy%208ball%20labda%20vagyok,%20nem%20foglalkozok%20a%20szar%20labdáiddal",
-                "biztos%20forrásból%20tudom:%20nem",
-                "biztos%20forrásból%20tudom:%20igen"
-            ]
-            # https://embed.rauf.wtf/?&author=%7B%7D&color=171A1B
-            await dc.send(msg, "https://embed.rauf.wtf/?&author={}&color=171A1B".format(
-                lista8[
-                    random.randrange(
-                        len(lista8)
-                    )
+            # 8ball
+            if dc.cmd(prefix, "8ball", msg):
+                lista8 = [
+                    "ez%20pokolian%20nem",
+                    "őszintén%20szólva%20nem%20érdekel%20lol",
+                    "nem%20vagyok%20benne%20biztos,%20de te biztos,%20hogy%20hülye%20vagy",
+                    "igen???",
+                    "amikor%20növesztessz%20egy%20agysejtet,%20akkor%20igen",
+                    "nem!!!!",
+                    "lol%20szó%20szerint%20nem",
+                    "a%20fenébe!%20nem.",
+                    "persze%20miért%20ne",
+                    "nem%20lmfao",
+                    "persze,%20engem%20se%20érdekel%20jobban",
+                    "Trump%20színe%20narancssárga?",
+                    "én%20egy%208ball%20labda%20vagyok,%20nem%20foglalkozok%20a%20szar%20labdáiddal",
+                    "biztos%20forrásból%20tudom:%20nem",
+                    "biztos%20forrásból%20tudom:%20igen"
                 ]
-            ))
+                # https://embed.rauf.wtf/?&author=%7B%7D&color=171A1B
+                await dc.send(msg, "https://embed.rauf.wtf/?&author={}&color=171A1B".format(
+                    lista8[
+                        random.randrange(
+                            len(lista8)
+                        )
+                    ]
+                ))
 
-    client.run(os.environ['BOT_TOKEN'])
-"""
-except Exception as e:
-    print("\n\n")
-    print("[ERROR]")
-    print(f"Error code: {str(e)}")
-    print("Restarting...")
-"""
+        client.run(os.environ['BOT_TOKEN'])
+
+    except Exception as e:
+        print("\n\n")
+        print("[ERROR]")
+        print(f"Error code: {str(e)}")
+        print("Restarting...")
