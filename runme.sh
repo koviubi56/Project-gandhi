6979b759cfa6c6bb99ba2238476ae71a2034ec87
koviubi56 <kmlenke1976@gmail.com>
2021-05-09 14:40:57 +0200

    ➕ runme.sh

---
+++ runme.sh
@@ -0,0 +1,13 @@
+echo UPTADING PIP
+pip install -U pip
+echo INSTALLING REQUESTS
+pip install requests
+echo Checking requests
+pip check requests
+echo INSTALLING DISCORD
+pip install discord
+echo Checking discord
+pip check discord
+echo RUNNING main.py
+python main.py
+echo ...END OF RUNNING main.py...
