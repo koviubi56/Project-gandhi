1306a79c4646516163279080ae10a127ec44b683
koviubi56 <koviubi56@protonmail.com>
2021-06-13 17:45:09 +0200

    ➕ r/aww

--- gckep.py
+++ gckep.py
@@ -17,14 +17,14 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import dc
-from random import choice
+from random import choice, randint
 
 lastId = 0
 reportok = []
 
 
 def getText(prefix: str) -> str:
-    url = choice([
+    listUn = [
         "https://source.unsplash.com/collection/1489913",
         "https://source.unsplash.com/collection/992002",
         "https://source.unsplash.com/collection/3473478",
@@ -55,9 +55,60 @@ def getText(prefix: str) -> str:
         "https://source.unsplash.com/collection/3226124",
         "https://source.unsplash.com/collection/2204857",
         "https://source.unsplash.com/collection/11381651",
+        "https://source.unsplash.com/collection/4488725",
+        "https://source.unsplash.com/collection/4558113",
+        "https://source.unsplash.com/collection/2271561",
+        "https://source.unsplash.com/collection/60923096",
+        "https://source.unsplash.com/collection/8263951",
+        "https://source.unsplash.com/collection/3143613",
+        "https://source.unsplash.com/collection/3661977",
+        "https://source.unsplash.com/collection/2270089",
+        "https://source.unsplash.com/collection/11353054",
+        "https://source.unsplash.com/collection/53728454",
+        "https://source.unsplash.com/collection/1489292",
+        "https://source.unsplash.com/collection/97648588",
+        "https://source.unsplash.com/collection/2034865",
+        "https://source.unsplash.com/collection/2047596",
+        "https://source.unsplash.com/collection/2298069",
+        "https://source.unsplash.com/collection/LDQLyupZ4YE",
+        "https://source.unsplash.com/collection/8981289",
+        "https://source.unsplash.com/collection/8658440",
+        "https://source.unsplash.com/collection/4966708",
+        "https://source.unsplash.com/collection/9290561",
+        "https://source.unsplash.com/collection/228402",
+        "https://source.unsplash.com/collection/23517384",
+        "https://source.unsplash.com/collection/4635928",
+        "https://source.unsplash.com/collection/8781437",
+        "https://source.unsplash.com/collection/9451935",
+        "https://source.unsplash.com/collection/1131206",
+        "https://source.unsplash.com/collection/6797651",
+        "https://source.unsplash.com/collection/2558692",
+        "https://source.unsplash.com/collection/8242570",
+        "https://source.unsplash.com/collection/1072946",
+        "https://source.unsplash.com/collection/4992797",
+        "https://source.unsplash.com/collection/9617498",
+        "https://source.unsplash.com/collection/8297227",
         "https://source.unsplash.com/collection/949734"
-    ])
-    id = url[38:]
+    ]
+    if randint(0, 5) == 0:
+        import requests
+
+        def get_reddit(subreddit, listing, limit, timeframe):
+            try:
+                base_url = f'https://www.reddit.com/r/{subreddit}/{listing}.json?limit={limit}&t={timeframe}'
+                request = requests.get(base_url, headers={
+                    'User-agent': 'yourbot'})
+            except:
+                raise RuntimeError(f"{base_url=}; {request=}")
+            else:
+                return request.json()
+
+        r = get_reddit("aww", "random", "1", "hour")
+        url = r[0]["data"]["children"][0]["data"]["url"]
+        id = ""
+    else:
+        url = choice(listUn)
+        id = url[38:]
     global lastId
     lastId = id
     return f"{url} (`{prefix}kép report` | *{id}*)"
