2d05bf31199f433738536be8a6a446cfdda52bb1
koviubi56 <koviubi56@protonmail.com>
2021-06-15 14:16:13 +0200

    ➕ shiba

--- gckep.py
+++ gckep.py
@@ -23,89 +23,119 @@ lastId = 0
 reportok = []
 
 
-def getText(prefix: str) -> str:
-    listUn = [
-        "https://source.unsplash.com/collection/1489913",
-        "https://source.unsplash.com/collection/992002",
-        "https://source.unsplash.com/collection/3473478",
-        "https://source.unsplash.com/collection/1758079",
-        "https://source.unsplash.com/collection/307400",
-        "https://source.unsplash.com/collection/9278231",
-        "https://source.unsplash.com/collection/8514212",
-        "https://source.unsplash.com/collection/4408740",
-        "https://source.unsplash.com/collection/9251157",
-        "https://source.unsplash.com/collection/17935256",
-        "https://source.unsplash.com/collection/85013168",
-        "https://source.unsplash.com/collection/1444235",
-        "https://source.unsplash.com/collection/5058951",
-        "https://source.unsplash.com/collection/8571458",
-        "https://source.unsplash.com/collection/805851",
-        "https://source.unsplash.com/collection/72016433",
-        "https://source.unsplash.com/collection/4356106",
-        "https://source.unsplash.com/collection/178050",
-        "https://source.unsplash.com/collection/3657124",
-        "https://source.unsplash.com/collection/8596141",
-        "https://source.unsplash.com/collection/584209",
-        "https://source.unsplash.com/collection/312000",
-        "https://source.unsplash.com/collection/1244701",
-        "https://source.unsplash.com/collection/8466901",
-        "https://source.unsplash.com/collection/1291369",
-        "https://source.unsplash.com/collection/976731",
-        "https://source.unsplash.com/collection/17823258",
-        "https://source.unsplash.com/collection/3226124",
-        "https://source.unsplash.com/collection/2204857",
-        "https://source.unsplash.com/collection/11381651",
-        "https://source.unsplash.com/collection/4488725",
-        "https://source.unsplash.com/collection/4558113",
-        "https://source.unsplash.com/collection/2271561",
-        "https://source.unsplash.com/collection/60923096",
-        "https://source.unsplash.com/collection/8263951",
-        "https://source.unsplash.com/collection/3143613",
-        "https://source.unsplash.com/collection/3661977",
-        "https://source.unsplash.com/collection/2270089",
-        "https://source.unsplash.com/collection/11353054",
-        "https://source.unsplash.com/collection/53728454",
-        "https://source.unsplash.com/collection/1489292",
-        "https://source.unsplash.com/collection/97648588",
-        "https://source.unsplash.com/collection/2034865",
-        "https://source.unsplash.com/collection/2047596",
-        "https://source.unsplash.com/collection/2298069",
-        "https://source.unsplash.com/collection/LDQLyupZ4YE",
-        "https://source.unsplash.com/collection/8981289",
-        "https://source.unsplash.com/collection/8658440",
-        "https://source.unsplash.com/collection/4966708",
-        "https://source.unsplash.com/collection/9290561",
-        "https://source.unsplash.com/collection/228402",
-        "https://source.unsplash.com/collection/23517384",
-        "https://source.unsplash.com/collection/4635928",
-        "https://source.unsplash.com/collection/8781437",
-        "https://source.unsplash.com/collection/9451935",
-        "https://source.unsplash.com/collection/1131206",
-        "https://source.unsplash.com/collection/6797651",
-        "https://source.unsplash.com/collection/2558692",
-        "https://source.unsplash.com/collection/8242570",
-        "https://source.unsplash.com/collection/1072946",
-        "https://source.unsplash.com/collection/4992797",
-        "https://source.unsplash.com/collection/9617498",
-        "https://source.unsplash.com/collection/8297227",
-        "https://source.unsplash.com/collection/949734"
-    ]
-    if randint(0, 2) != 0:
-        import requests
+def getText(what: str, prefix: str) -> str:
+    if what.lower() == "cute":
+        listUn = [
+            "https://source.unsplash.com/collection/1489913",
+            "https://source.unsplash.com/collection/992002",
+            "https://source.unsplash.com/collection/3473478",
+            "https://source.unsplash.com/collection/1758079",
+            "https://source.unsplash.com/collection/307400",
+            "https://source.unsplash.com/collection/9278231",
+            "https://source.unsplash.com/collection/8514212",
+            "https://source.unsplash.com/collection/4408740",
+            "https://source.unsplash.com/collection/9251157",
+            "https://source.unsplash.com/collection/17935256",
+            "https://source.unsplash.com/collection/85013168",
+            "https://source.unsplash.com/collection/1444235",
+            "https://source.unsplash.com/collection/5058951",
+            "https://source.unsplash.com/collection/8571458",
+            "https://source.unsplash.com/collection/805851",
+            "https://source.unsplash.com/collection/72016433",
+            "https://source.unsplash.com/collection/4356106",
+            "https://source.unsplash.com/collection/178050",
+            "https://source.unsplash.com/collection/3657124",
+            "https://source.unsplash.com/collection/8596141",
+            "https://source.unsplash.com/collection/584209",
+            "https://source.unsplash.com/collection/312000",
+            "https://source.unsplash.com/collection/1244701",
+            "https://source.unsplash.com/collection/8466901",
+            "https://source.unsplash.com/collection/1291369",
+            "https://source.unsplash.com/collection/976731",
+            "https://source.unsplash.com/collection/17823258",
+            "https://source.unsplash.com/collection/3226124",
+            "https://source.unsplash.com/collection/2204857",
+            "https://source.unsplash.com/collection/11381651",
+            "https://source.unsplash.com/collection/4488725",
+            "https://source.unsplash.com/collection/4558113",
+            "https://source.unsplash.com/collection/2271561",
+            "https://source.unsplash.com/collection/60923096",
+            "https://source.unsplash.com/collection/8263951",
+            "https://source.unsplash.com/collection/3143613",
+            "https://source.unsplash.com/collection/3661977",
+            "https://source.unsplash.com/collection/2270089",
+            "https://source.unsplash.com/collection/11353054",
+            "https://source.unsplash.com/collection/53728454",
+            "https://source.unsplash.com/collection/1489292",
+            "https://source.unsplash.com/collection/97648588",
+            "https://source.unsplash.com/collection/2034865",
+            "https://source.unsplash.com/collection/2047596",
+            "https://source.unsplash.com/collection/2298069",
+            "https://source.unsplash.com/collection/LDQLyupZ4YE",
+            "https://source.unsplash.com/collection/8981289",
+            "https://source.unsplash.com/collection/8658440",
+            "https://source.unsplash.com/collection/4966708",
+            "https://source.unsplash.com/collection/9290561",
+            "https://source.unsplash.com/collection/228402",
+            "https://source.unsplash.com/collection/23517384",
+            "https://source.unsplash.com/collection/4635928",
+            "https://source.unsplash.com/collection/8781437",
+            "https://source.unsplash.com/collection/9451935",
+            "https://source.unsplash.com/collection/1131206",
+            "https://source.unsplash.com/collection/6797651",
+            "https://source.unsplash.com/collection/2558692",
+            "https://source.unsplash.com/collection/8242570",
+            "https://source.unsplash.com/collection/1072946",
+            "https://source.unsplash.com/collection/4992797",
+            "https://source.unsplash.com/collection/9617498",
+            "https://source.unsplash.com/collection/8297227",
+            "https://source.unsplash.com/collection/949734"
+        ]
+        if randint(0, 2) != 0:
+            import requests
 
-        def get_reddit(subreddit, listing, limit, timeframe):
-            try:
+            def get_reddit(subreddit, listing, limit, timeframe):
                 base_url = f'https://www.reddit.com/r/{subreddit}/{listing}.json?limit={limit}&t={timeframe}'
                 request = requests.get(base_url, headers={
                     'User-agent': 'yourbot'})
-            except:
-                raise RuntimeError
-            else:
                 return request.json()
-        global r
+            global r
+            while True:
+                try:
+                    r = get_reddit("aww", "random", "1", "hour")
+                    try:
+                        url = r[0]["data"]["children"][0]["data"]["secure_media"]["reddit_video"]["fallback_url"]
+                    except TypeError:
+                        url = r[0]["data"]["children"][0]["data"]["url_overridden_by_dest"]
+                    except KeyError:
+                        try:
+                            url = r[0]["data"]["children"][0]["data"]["secure_media"]["oembed"]["url"]
+                        except KeyError:
+                            url = r[0]["data"]["children"][0]["data"]["secure_media"]["oembed"]["thumbnail_url"]
+                    id = "{}/{}".format(r[0]["data"]["children"][0]["data"]["subreddit_name_prefixed"],
+                                        url)
+                except:
+                    continue
+                else:
+                    break
+        else:
+            url = choice(listUn)
+            id = f"unsplash/cute/{url[38:]}"
+        global lastId
+        lastId = id
+        return f"{url} (`{prefix}kép report` | *{id}*)"
+    elif what == "shiba":
+        import requests
+
+        def get_reddit(subreddit, listing, limit, timeframe):
+            base_url = f'https://www.reddit.com/r/{subreddit}/{listing}.json?limit={limit}&t={timeframe}'
+            request = requests.get(base_url, headers={
+                'User-agent': 'yourbot'})
+            return request.json()
+        #global r
         while True:
             try:
-                r = get_reddit("aww", "random", "1", "hour")
+                r = get_reddit("shiba", "random", "1", "hour")
                 try:
                     url = r[0]["data"]["children"][0]["data"]["secure_media"]["reddit_video"]["fallback_url"]
                 except TypeError:
@@ -115,18 +145,18 @@ def getText(prefix: str) -> str:
                         url = r[0]["data"]["children"][0]["data"]["secure_media"]["oembed"]["url"]
                     except KeyError:
                         url = r[0]["data"]["children"][0]["data"]["secure_media"]["oembed"]["thumbnail_url"]
-                id = "{}".format(r[0]["data"]["children"][0]
-                                 ["data"]["subreddit_name_prefixed"])
+                id = "{}/{}".format(r[0]["data"]["children"][0]["data"]["subreddit_name_prefixed"],
+                                    url)
             except:
                 continue
             else:
                 break
+        #global lastId
+        lastId = id
+        return f"{url} (`{prefix}kép report` | *{id}*)"
     else:
-        url = choice(listUn)
-        id = f"unsplash/cute/{url[38:]}"
-    global lastId
-    lastId = id
-    return f"{url} (`{prefix}kép report` | *{id}*)"
+        raise ValueError("{} is not in this list: {}".format(
+            what, str(["cute", "shiba"])))
 
 
 async def main(msg, prefix):
@@ -134,7 +164,10 @@ async def main(msg, prefix):
     if len(msg.content) > len(f"{prefix}kép "):
         kwd = msg.content[len(f"{prefix}kép "):]
         if kwd == "cute":
-            await dc.send(msg, getText(prefix))
+            await dc.send(msg, getText("cute", prefix))
+        elif kwd == "shiba":
+            await dc.send(msg, getText("shiba", prefix))
+
         elif kwd == "report":
             global lastId
             reportok.append({"id": lastId, "bejelento": str(msg.author)})
@@ -144,6 +177,6 @@ async def main(msg, prefix):
             await dc.send(msg, f"```py\n{reportok}\n```")
         else:
             id = f"unsplash/custom/{kwd}"
-            await dc.send(msg, f"https://source.unsplash.com/featured/?{kwd} ({id})")
+            await dc.send(msg, f"https://source.unsplash.com/featured/?{kwd} (*{id}*)")
     else:
         await dc.send(msg, "EEEEEEE! Oszt mit mutassak?!")
