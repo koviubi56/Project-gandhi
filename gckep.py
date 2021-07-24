811b4804c5687f755db83e8f9e0b18621c5bc555
koviubi56 <58398849+koviubi56@users.noreply.github.com>
2021-07-24 19:18:46 +0200

    Update gckep.py

--- gckep.py
+++ gckep.py
@@ -16,7 +16,6 @@ You should have received a copy of the GNU Affero General Public License
 along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
-import dc
 from random import choice
 from discord_components import DiscordComponents, Button
 from functools import lru_cache
