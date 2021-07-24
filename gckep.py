5463e30ab92e0a9ed06c2d78167bec10404147fa
koviubi56 <58398849+koviubi56@users.noreply.github.com>
2021-07-24 05:55:11 +0000

    L

--- gckep.py
+++ gckep.py
@@ -17,133 +17,74 @@ along with this program.  If not, see <https://www.gnu.org/licenses/>.
 """
 
 import dc
-from random import choice, randint
+from random import choice
 from discord_components import DiscordComponents, Button
 from functools import lru_cache
+import aiohttp
+from asyncio import sleep
 
-def getText(what: str) -> str:
-    if what.lower() == "cute":
-        listUn = [
-            "https://source.unsplash.com/collection/1489913",
-            "https://source.unsplash.com/collection/992002",
-            "https://source.unsplash.com/collection/3473478",
-            "https://source.unsplash.com/collection/1758079",
-            "https://source.unsplash.com/collection/307400",
-            "https://source.unsplash.com/collection/9278231",
-            "https://source.unsplash.com/collection/8514212",
-            "https://source.unsplash.com/collection/4408740",
-            "https://source.unsplash.com/collection/9251157",
-            "https://source.unsplash.com/collection/17935256",
-            "https://source.unsplash.com/collection/85013168",
-            "https://source.unsplash.com/collection/1444235",
-            "https://source.unsplash.com/collection/5058951",
-            "https://source.unsplash.com/collection/8571458",
-            "https://source.unsplash.com/collection/805851",
-            "https://source.unsplash.com/collection/72016433",
-            "https://source.unsplash.com/collection/4356106",
-            "https://source.unsplash.com/collection/178050",
-            "https://source.unsplash.com/collection/3657124",
-            "https://source.unsplash.com/collection/8596141",
-            "https://source.unsplash.com/collection/584209",
-            "https://source.unsplash.com/collection/312000",
-            "https://source.unsplash.com/collection/1244701",
-            "https://source.unsplash.com/collection/8466901",
-            "https://source.unsplash.com/collection/1291369",
-            "https://source.unsplash.com/collection/976731",
-            "https://source.unsplash.com/collection/17823258",
-            "https://source.unsplash.com/collection/3226124",
-            "https://source.unsplash.com/collection/2204857",
-            "https://source.unsplash.com/collection/11381651",
-            "https://source.unsplash.com/collection/4488725",
-            "https://source.unsplash.com/collection/4558113",
-            "https://source.unsplash.com/collection/2271561",
-            "https://source.unsplash.com/collection/60923096",
-            "https://source.unsplash.com/collection/8263951",
-            "https://source.unsplash.com/collection/3143613",
-            "https://source.unsplash.com/collection/3661977",
-            "https://source.unsplash.com/collection/2270089",
-            "https://source.unsplash.com/collection/11353054",
-            "https://source.unsplash.com/collection/53728454",
-            "https://source.unsplash.com/collection/1489292",
-            "https://source.unsplash.com/collection/97648588",
-            "https://source.unsplash.com/collection/2034865",
-            "https://source.unsplash.com/collection/2047596",
-            "https://source.unsplash.com/collection/2298069",
-            "https://source.unsplash.com/collection/LDQLyupZ4YE",
-            "https://source.unsplash.com/collection/8981289",
-            "https://source.unsplash.com/collection/8658440",
-            "https://source.unsplash.com/collection/4966708",
-            "https://source.unsplash.com/collection/9290561",
-            "https://source.unsplash.com/collection/228402",
-            "https://source.unsplash.com/collection/23517384",
-            "https://source.unsplash.com/collection/4635928",
-            "https://source.unsplash.com/collection/8781437",
-            "https://source.unsplash.com/collection/9451935",
-            "https://source.unsplash.com/collection/1131206",
-            "https://source.unsplash.com/collection/6797651",
-            "https://source.unsplash.com/collection/2558692",
-            "https://source.unsplash.com/collection/8242570",
-            "https://source.unsplash.com/collection/1072946",
-            "https://source.unsplash.com/collection/4992797",
-            "https://source.unsplash.com/collection/9617498",
-            "https://source.unsplash.com/collection/8297227",
-            "https://source.unsplash.com/collection/949734"
-        ]
-        if randint(0, 2) != 0:
-            import requests
+class MyErros:
+    class StatusCodeError(Exception): pass
 
-            def get_reddit(subreddit, listing, limit, timeframe):
-                base_url = f'https://www.reddit.com/r/{subreddit}/{listing}.json?limit={limit}&t={timeframe}'
-                request = requests.get(base_url, headers={
-                    'User-agent': 'yourbot'})
-                return request.json()
-            while True:
-                try:
-                    global r
-                    r = get_reddit("aww", "random", "1", "hour")
-                    try:
-                        url = r[0]["data"]["children"][0]["data"]["secure_media"]["reddit_video"]["fallback_url"]
-                    except TypeError:
-                        url = r[0]["data"]["children"][0]["data"]["url_overridden_by_dest"]
-                    except KeyError:
-                        try:
-                            url = r[0]["data"]["children"][0]["data"]["secure_media"]["oembed"]["url"]
-                        except KeyError:
-                            url = r[0]["data"]["children"][0]["data"]["secure_media"]["oembed"]["thumbnail_url"]
-                    id = "{}".format(r[0]["data"]["children"][0]["data"]["subreddit_name_prefixed"])
-                except Exception:
-                    continue
-                else:
-                    break
-        else:
-            url = choice(listUn)
-        return f"{url}"
-    elif what == "shiba":
-        import requests
+async def get_reddit(subreddit, listing, limit, timeframe):
+    base_url = f'https://www.reddit.com/r/{subreddit}/{listing}.json?limit={limit}&t={timeframe}'
+    async with aiohttp.ClientSession() as session:
+        async with session.get(base_url) as r:
+            if r.status < 400:  # OK
+                return await r.json()
+            raise MyErrors.StatusCodeError("r.status is {}".format(str(r.status)))
 
-        def get_reddit(subreddit, listing, limit, timeframe):
-            base_url = f'https://www.reddit.com/r/{subreddit}/{listing}.json?limit={limit}&t={timeframe}'
-            request = requests.get(base_url, headers={
-                'User-agent': 'yourbot'})
-            return request.json()
-        while True:
+def myfind(inWhat: str, forWhat: tuple):
+    for j in forWhat:
+        if inWhat.find(j) != -1:
+            return True
+    return False
+
+async def getText(what: str) -> str:
+    cuteSubs = [
+        "aww",
+        "Eyebleach",
+        "Blep",
+        "brushybrushy",
+        "catpictures",
+        "AnimalsBeingBros"
+    ]
+    # MAX várakozás
+    # SLEEPTIME | Sec
+    # --------- | ---
+    # 0.02      | 1
+    # 0.04      | 2
+    # 0.055     | 2.75
+    # COMPONENT-NEK
+    # 3 MP-EN BELÜL
+    # VÁLASZOLNIA KELL
+    SLEEPTIME = 0.055
+    for x in range(50):
+        try:
+            global r
+            r = await get_reddit(choice(cuteSubs) if what is "cute" else "shiba", "random", "1", "day")
             try:
-                #global r
-                r = get_reddit("shiba", "random", "1", "hour")
+                url = r[0]["data"]["children"][0]["data"]["secure_media"]["reddit_video"]["fallback_url"]
+            except TypeError:
+                url = r[0]["data"]["children"][0]["data"]["url_overridden_by_dest"]
+            except KeyError:
                 try:
-                    url = r[0]["data"]["children"][0]["data"]["secure_media"]["reddit_video"]["fallback_url"]
-                except TypeError:
-                    url = r[0]["data"]["children"][0]["data"]["url_overridden_by_dest"]
+                    url = r[0]["data"]["children"][0]["data"]["secure_media"]["oembed"]["url"]
                 except KeyError:
-                    try:
-                        url = r[0]["data"]["children"][0]["data"]["secure_media"]["oembed"]["url"]
-                    except KeyError:
-                        url = r[0]["data"]["children"][0]["data"]["secure_media"]["oembed"]["thumbnail_url"]
-            except Exception:
+                    url = r[0]["data"]["children"][0]["data"]["secure_media"]["oembed"]["thumbnail_url"]
+            if myfind(url, ("gallery", "people.com")):
+                sleep(SLEEPTIME)
                 continue
-            else:
-                break
-        return f"{url}"
+        except Exception:
+            sleep(SLEEPTIME)
+            continue
+        else:
+            sub = r[0]["data"]["children"][0]["data"]["subreddit_name_prefixed"]
+            tries = x + 1
+            break
+    else:
+        return "**HIBA!** 50-szer próbálkoztunk, de nem találnunk egy képet. *Sad Gamdhi noises*"
+    return f"{url} `({sub} | {tries})`"
 
 @lru_cache
 def comp(s1: int, s2: int, d1=False, d2=False):
@@ -155,11 +96,15 @@ async def main(msg, prefix, client):
         if i.responded:
             return
         iid = i.component.id
-        if iid == "kepCute":
-            await i.respond(content=getText("cute"), components=comp(s1=1, s2=2))
-        elif iid == "kepShiba":
-            await i.respond(content=getText("shiba"), components=comp(s1=2, s2=1))
+        try:
+            if iid == "kepCute":
+                await i.respond(content=await getText("cute"), components=comp(s1=1, s2=2))
+            elif iid == "kepShiba":
+                await i.respond(content=await getText("shiba"), components=comp(s1=2, s2=1))
+        except MyErrors.StatusCodeError as e:
+            await i.respond("**Hiba!** `({})`".format(e))
         await kuld()
+
 
     global c
     c = client
